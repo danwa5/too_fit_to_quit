131bde8f30b4586f7e194ffa33ff5c335a809702
Daniel Lee <dlee@sheetmusicplus.com>
2016-07-18 21:28:02 -0700

    Add date parameter to steps form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,27 @@
 class StaticPagesController < ApplicationController
 
   def index
-    if params[:steps].present?
-      results = FitbitService.get_steps(Identity.first, {period: params[:steps]})
+    @options = {}
+    if steps_params[:steps].present?
+      date = parse_date(steps_params[:date])
+      @options = {period: steps_params[:steps]}
+      @options.merge!({date: date}) if date.present?
+      results = FitbitService.get_steps(Identity.first, @options)
       @steps_data = results.parsed_response['activities-steps']
     end
   end
 
+  private
+
+  def steps_params
+    params.permit(:steps, :date)
+  end
+
+  def parse_date(date)
+    begin
+      Date.parse(date.to_s).strftime('%Y-%m-%d')
+    rescue ArgumentError
+      nil
+    end
+  end
 end
