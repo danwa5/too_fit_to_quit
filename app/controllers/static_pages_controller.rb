6675a0dcfea2fa05c2436a2ca9a6ba009438dc7d
Daniel Lee <dlee@sheetmusicplus.com>
2016-07-21 16:49:43 -0700

    Add form to query and display run data

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,8 @@ class StaticPagesController < ApplicationController
 
   def index
     @options = {}
+    @runs_options = { }
+
     if steps_params[:steps].present?
       date = parse_date(steps_params[:date])
       @options = {period: steps_params[:steps]}
@@ -13,12 +15,32 @@ class StaticPagesController < ApplicationController
         flash[:alert] = '<a href="/users/auth/fitbit_oauth2">Please request authorization from Fitbit to access your private data</a>.'.html_safe
       end
     end
+
+    if runs_params[:after_date].present?
+      date = parse_date(runs_params[:after_date])
+      @runs_options = { date: date }
+      results = FitbitService.get_activities_list(current_user.fitbit_identity, @runs_options).parsed_response
+
+      if results['success'] == false
+        flash[:alert] = '<a href="/users/auth/fitbit_oauth2">Please request authorization from Fitbit to access your private data</a>.'.html_safe
+      else
+        @runs_data = []
+        results['activities'].each do |activity_hash|
+          @runs_data << activity_hash if activity_hash['activityName'] == 'Run'
+        end
+        render json: @runs_data
+      end
+    end
   end
 
   private
 
   def steps_params
     params.permit(:steps, :date)
+  end
+
+  def runs_params
+    params.permit(:after_date)
   end
 
   def parse_date(date)
--- app/services/fitbit_service.rb
+++ app/services/fitbit_service.rb
@@ -5,22 +5,28 @@ class FitbitService
 
   format :json
 
-  def self.get_profile(client_identity)
-    new(client_identity).get_profile
-  end
+  class << self
+    def get_profile(client_identity)
+      new(client_identity).get_profile
+    end
 
-  def self.get_steps(client_identity, options={})
-    defaults = {
-      :date   => Date.today.strftime('%Y-%m-%d'),
-      :period => '1d'
-    }
-    options = defaults.merge(options)
-    new(client_identity, options).get_steps
-  end
+    def get_steps(client_identity, options={})
+      defaults = {
+        :date   => Date.today.strftime('%Y-%m-%d'),
+        :period => '1d'
+      }
+      options = defaults.merge(options)
+      new(client_identity, options).get_steps
+    end
 
-  # def self.get_request(client_identity, options)
-  #   new(client_identity, options).get_request
-  # end
+    def get_activities_list(client_identity, options={})
+      defaults = {
+        :date => Date.today.strftime('%Y-%m-%d')
+      }
+      options = defaults.merge(options)
+      new(client_identity, options).get_activities_list
+    end
+  end
 
   def initialize(client_identity, options={})
     @client_token = client_identity.access_token
@@ -36,6 +42,11 @@ class FitbitService
   # GET https://api.fitbit.com/1/user/[user-id]/activities/steps/date/today/1m.json
   def get_steps
     self.class.get(Figaro.env.fitbit_api_url + "/1/user/#{@client_uid}/activities/steps/date/#{@options[:date]}/#{@options[:period]}.json", headers: headers)
+  end
+
+  # GET https://api.fitbit.com/1/user/-/activities/list.json
+  def get_activities_list
+    self.class.get(Figaro.env.fitbit_api_url + "/1/user/#{@client_uid}/activities/list.json?afterDate=#{@options[:date]}&sort=asc&limit=20&offset=0", headers: headers)
   end
 
   private
