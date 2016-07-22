6c5f4101d72ba993dafd99ae5b32a65151cf2025
Daniel Lee <dlee@sheetmusicplus.com>
2016-07-21 21:40:21 -0700

    Add and configure simplecov for test coverage

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
+require 'simplecov' if ENV["COVERAGE"]
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
@@ -56,6 +57,8 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+WebMock.disable_net_connect!(:allow_localhost => true)
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
