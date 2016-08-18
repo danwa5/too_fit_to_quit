8c6d1ffcbf646211ba8c7dbe02ee2f81c3f7dba5
Daniel Lee <dlee@sheetmusicplus.com>
2016-08-18 00:36:14 -0700

    Minor changes

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,25 +1,23 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   
   def fitbit_oauth2
-    @user = User.from_omniauth(request.env['omniauth.auth'], current_user)
-
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => 'Fitbit') if is_navigational_format?
-    else
-      session['devise.fitbit_data'] = request.env['omniauth.auth']
-      redirect_to new_user_registration_url
-    end
+    authenticate('Fitbit')
   end
 
   def strava
+    authenticate('Strava')
+  end
+
+  private
+
+  def authenticate(provider)
     @user = User.from_omniauth(request.env['omniauth.auth'], current_user)
 
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => 'Strava') if is_navigational_format?
+      set_flash_message(:notice, :success, :kind => provider) if is_navigational_format?
     else
-      session['devise.strava_data'] = request.env['omniauth.auth']
+      session["devise.#{provider.downcase}_data"] = request.env['omniauth.auth']
       redirect_to new_user_registration_url
     end
   end
