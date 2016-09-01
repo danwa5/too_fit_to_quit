1b2d7e7fa847d19f63a419fcfe733068c91d7e63
Daniel Lee <dlee@sheetmusicplus.com>
2016-08-31 23:18:13 -0700

    Add react-rails gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,4 +40,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Sets React.js build
+  config.react.variant = :development
 end
