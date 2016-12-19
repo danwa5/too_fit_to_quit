ee37be23e005ba979e6c7f5a8b823b0c879b08fc
Daniel Lee <dlee@sheetmusicplus.com>
2016-12-19 14:40:21 -0800

    Display both current and previous year in monthly breakdown chart

--- app/models/user_activity.rb
+++ app/models/user_activity.rb
@@ -9,11 +9,17 @@ class UserActivity < ActiveRecord::Base
     find_by_sql("
       select * from (select * from generate_series(1,12) month) month
       left join (
-        select extract(month from start_time) as month, count(id) as total
+        select extract(month from start_time) as month, count(id) as total_#{year-1}
+        from user_activities
+        where activity_type = 'Activity::FitbitRun' and date_part('year', start_time) = #{year-1}
+        group by month
+      ) ua using (month)
+      left join (
+        select extract(month from start_time) as month, count(id) as total_#{year}
         from user_activities
         where activity_type = 'Activity::FitbitRun' and date_part('year', start_time) = #{year}
         group by month
-      ) ua using (month)
+      ) ua2 using (month)
       order by month;"
     ).as_json
   }
--- spec/models/user_activity_spec.rb
+++ spec/models/user_activity_spec.rb
@@ -13,40 +13,34 @@ RSpec.describe UserActivity, type: :model do
 
   describe '.monthly_breakdown' do
     let(:user) { create(:user) }
+    let(:year) { Date.today.year }
     before do
       create(:user_activity, :fitbit, user: user, start_time: Faker::Date.between('2009-01-01', '2009-12-31'))
-      create(:user_activity, :fitbit, user: user, start_time: "#{Date.today.year}-06-01")
-      create(:user_activity, :fitbit, user: user, start_time: "#{Date.today.year}-08-01")
-      create(:user_activity, :fitbit, user: user, start_time: "#{Date.today.year}-08-08")
+      create(:user_activity, :fitbit, user: user, start_time: "#{year-1}-06-30")
+      create(:user_activity, :fitbit, user: user, start_time: "#{year}-06-01")
+      create(:user_activity, :fitbit, user: user, start_time: "#{year}-08-01")
+      create(:user_activity, :fitbit, user: user, start_time: "#{year}-08-08")
     end
-    it 'returns a monthly breakdown of run count for the current year' do
+    it 'returns a monthly breakdown of run count for the current year and the previous year' do
       res = described_class.monthly_breakdown
       aggregate_failures 'json data' do
         expect(res.count).to eq(12)
-        expect(res[0]['month']).to eq(1)
-        expect(res[0]['total']).to be_nil
-        expect(res[1]['month']).to eq(2)
-        expect(res[1]['total']).to be_nil
-        expect(res[2]['month']).to eq(3)
-        expect(res[2]['total']).to be_nil
-        expect(res[3]['month']).to eq(4)
-        expect(res[3]['total']).to be_nil
-        expect(res[4]['month']).to eq(5)
-        expect(res[4]['total']).to be_nil
-        expect(res[5]['month']).to eq(6)
-        expect(res[5]['total']).to eq(1)
-        expect(res[6]['month']).to eq(7)
-        expect(res[6]['total']).to be_nil
-        expect(res[7]['month']).to eq(8)
-        expect(res[7]['total']).to eq(2)
-        expect(res[8]['month']).to eq(9)
-        expect(res[8]['total']).to be_nil
-        expect(res[9]['month']).to eq(10)
-        expect(res[9]['total']).to be_nil
-        expect(res[10]['month']).to eq(11)
-        expect(res[10]['total']).to be_nil
-        expect(res[11]['month']).to eq(12)
-        expect(res[11]['total']).to be_nil
+        expect(res[0]["month"]).to eq(1)
+        expect(res[1]["month"]).to eq(2)
+        expect(res[2]["month"]).to eq(3)
+        expect(res[3]["month"]).to eq(4)
+        expect(res[4]["month"]).to eq(5)
+        expect(res[5]["month"]).to eq(6)
+        expect(res[6]["month"]).to eq(7)
+        expect(res[7]["month"]).to eq(8)
+        expect(res[8]["month"]).to eq(9)
+        expect(res[9]["month"]).to eq(10)
+        expect(res[10]["month"]).to eq(11)
+        expect(res[11]["month"]).to eq(12)
+
+        expect(res[5]["total_#{year-1}"]).to eq(1)
+        expect(res[5]["total_#{year}"]).to eq(1)
+        expect(res[7]["total_#{year}"]).to eq(2)
       end
     end
   end
