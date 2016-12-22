3dffecbb2ee565d62e62ddf6af6427b1b8839ec4
Daniel Lee <dlee@sheetmusicplus.com>
2016-12-22 00:00:38 -0800

    Add city, state_province, and country attributes to Activity::FitbitRun

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161015065503) do
+ActiveRecord::Schema.define(version: 20161222064551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,9 @@ ActiveRecord::Schema.define(version: 20161015065503) do
     t.datetime "updated_at",       null: false
     t.json     "tcx_data"
     t.json     "gps_data"
+    t.string   "city"
+    t.string   "state_province"
+    t.string   "country"
   end
 
   add_index "activity_fitbit_runs", ["user_id"], name: "index_activity_fitbit_runs_on_user_id", using: :btree
