9837f82baaa4c4f21f05458d6e019607e6b11f32
Daniel Lee <dlee@sheetmusicplus.com>
2016-12-23 11:57:41 -0800

    Fix form css bug

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -2,10 +2,10 @@ module Searchable
   extend ActiveSupport::Concern
 
   included do
-    scope :matching_contain, ->(column, value) {
+    scope :matching_contain, ->(column, value) do
       return where(nil) unless value.present?
       where("#{column} like ?", "%#{value}%")
-    }
+    end
 
     scope :number_within_range, ->(column, low_value, high_value) do
       return where("#{column} >= ? AND #{column} <= ?", low_value, high_value) if !low_value.blank? && !high_value.blank?
