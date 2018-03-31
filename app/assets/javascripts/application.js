84ba2412d41648c05ea18cd6eca571bcbbd5554b
Daniel Lee <dlee@sheetmusicplus.com>
2018-03-30 22:57:58 -0700

    Update bootstrap to v4

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery2
+//= require jquery3
 //= require jquery_ujs
+//= require popper
 //= require bootstrap-sprockets
 //= require underscore
 //= require turbolinks
